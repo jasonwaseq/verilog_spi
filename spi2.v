64468fbcdf87d2e0cf57af4ebe3561325bdb4143
MrPoloGit <therealmr.polo1@gmail.com>
2026-05-05 14:06:34 -0700

    Fixes to reduce verilator errors

--- spi2.v
+++ spi2.v
@@ -11,7 +11,7 @@
 
 `define SPI_CLK_DIV 'sd2
 
-module fpga_top ( 
+module fpga_top (
 	input wire WF_CLK,
 	output wire SIGNAL_CLOCK_OUT,
 	output wire SIGNAL_SS_OUT,
@@ -24,10 +24,10 @@ module fpga_top (
 	//Local registers and wires
 	reg [7:0] resetn_counter;
 	wire resetn = &resetn_counter;
-	
+
 	reg reset_spi;
 	reg spi_ready;
-	
+
 	reg reset_spi2;
 	reg spi_ready2;
 
@@ -36,122 +36,135 @@ module fpga_top (
 	reg process_next_word;
 	reg process_next_word2;
 	reg [`SPI_WORD_LEN - 1:0] data;
-	
+
 	wire recv_new_word_present;
 	wire [`SPI_WORD_LEN - 1:0] recv_tmp;
 	reg [`SPI_WORD_LEN - 1:0] recv_data;
-		
+
 	reg test_signal_control;
 	assign TEST_SIGNAL_OUT = test_signal_control;
-	
+
 	reg reset_div;
 	reg divider_ready;
-	wire divided_master_clock; 
-	
+	wire divided_master_clock;
+
 	//Clock divider module
 	clock_divider #( .DIV_N(`SPI_CLK_DIV) )	clkdiv ( .clk_in(WF_CLK), .clk_out(divided_master_clock), .do_reset(reset_div), .is_ready(divider_ready) );
-	
-spi_module 
-	#( .SPI_MASTER (1'b1) )
-	spi_master
-	( .master_clock(WF_CLK),
-	.SCLK_OUT(SIGNAL_CLOCK_OUT),
-  	.SCLK_IN(divided_master_clock),
-  	.SS_OUT(SIGNAL_SS_OUT),
-  	.SS_IN(),
-	.OUTPUT_SIGNAL(SIGNAL_DATA_OUT),
-	.processing_word(proc_word), 
-	.process_next_word(process_next_word),
-	.data_word_send(data),
-	.INPUT_SIGNAL(),
-	.data_word_recv(),
-	.do_reset(reset_spi),
-	.is_ready(spi_ready) );
-	
-	spi_module 
-	#( .SPI_MASTER (1'b0) )
-	spi_slave
-	( .master_clock(WF_CLK),
-	.SCLK_OUT(),
-  	.SCLK_IN(SIGNAL_CLOCK_IN),
-  	.SS_OUT(),
-  	.SS_IN(SIGNAL_SS_IN),
-	.OUTPUT_SIGNAL(),
-	.processing_word(proc_word2), 
-	.process_next_word(process_next_word2),
-	.data_word_send(),
-	.INPUT_SIGNAL(SIGNAL_DATA_IN),
-	.data_word_recv(recv_tmp),
-	.do_reset(reset_spi2),
-	.is_ready(spi_ready2) );
+
+	spi_module #(
+        .SPI_MASTER (1'b1)
+	) spi_master (
+        .master_clock(WF_CLK),
+	    .SCLK_OUT(SIGNAL_CLOCK_OUT),
+  	    .SCLK_IN(divided_master_clock),
+  	    .SS_OUT(SIGNAL_SS_OUT),
+        /* verilator lint_off PINCONNECTEMPTY */
+  	    .SS_IN(),
+        /* verilator lint_on PINCONNECTEMPTY */
+	    .OUTPUT_SIGNAL(SIGNAL_DATA_OUT),
+	    .processing_word(proc_word),
+	    .process_next_word(process_next_word),
+	    .data_word_send(data),
+		/* verilator lint_off PINCONNECTEMPTY */
+	    .INPUT_SIGNAL(),
+	    .data_word_recv(),
+		/* verilator lint_on PINCONNECTEMPTY */
+	    .do_reset(reset_spi),
+	    .is_ready(spi_ready)
+	);
+
+	spi_module #(
+	    .SPI_MASTER (1'b0)
+	) spi_slave (
+	    .master_clock(WF_CLK),
+		/* verilator lint_off PINCONNECTEMPTY */
+	    .SCLK_OUT(),
+		/* verilator lint_on PINCONNECTEMPTY */
+  	    .SCLK_IN(SIGNAL_CLOCK_IN),
+        /* verilator lint_off PINCONNECTEMPTY */
+  	    .SS_OUT(),
+        /* verilator lint_on PINCONNECTEMPTY */
+  	    .SS_IN(SIGNAL_SS_IN),
+        /* verilator lint_off PINCONNECTEMPTY */
+	    .OUTPUT_SIGNAL(),
+		/* verilator lint_on PINCONNECTEMPTY */
+	    .processing_word(proc_word2),
+	    .process_next_word(process_next_word2),
+		/* verilator lint_off PINCONNECTEMPTY */
+	    .data_word_send(),
+		/* verilator lint_on PINCONNECTEMPTY */
+	    .INPUT_SIGNAL(SIGNAL_DATA_IN),
+	    .data_word_recv(recv_tmp),
+	    .do_reset(reset_spi2),
+	    .is_ready(spi_ready2)
+	);
 
 	always @(posedge WF_CLK) begin
-	
+
 		if (!resetn) begin
 			//do reset stuff
-			
+
 			reset_div <= 1'b1;
-		
+
 			process_next_word <= 1'b0;
-				
+
 			process_next_word2 <= 1'b0;
-		
+
 			reset_spi <= 1'b1;
-		
+
 			reset_spi2 <= 1'b1;
-		
+
 			data <= 'sd0;
-		
+
 			//data <= `TEST_BYTE;
-			
+
 			test_signal_control <= 1'b0;
-			
+
 			resetn_counter <= resetn_counter + 'd1;
 		end
 		else begin
-		
+
 		if(divider_ready) begin
 				reset_div <= 1'b0;
-			
+
 				if(spi_ready) begin
 					reset_spi <= 1'b0;
-					
+
 				if(spi_ready2) begin
 					reset_spi2 <= 1'b0;
-		
+
 					if(!proc_word) begin
-			
+
 						if(!process_next_word) begin
-			
+
 							if(data < 'sd15) data <= data + 'sd1;
 							else data <= 'sd0;
-		
+
 						end
-			
+
 						process_next_word <= 1'b1;
-			
+
 					end
 					else if (proc_word && process_next_word)  process_next_word <= 1'b0;
-		
+
 					if(!proc_word2) begin
-			
+
 						if(!process_next_word2) recv_data <= recv_tmp;
-			
+
 						process_next_word2 <= 1'b1;
-			
+
 					end
 					else if (proc_word2 && process_next_word2)  process_next_word2 <= 1'b0;
-			
+
 					if(recv_data == `TEST_BYTE_LED_ON) test_signal_control <= 1'b1;
 					else if(recv_data == `TEST_BYTE_LED_OFF) test_signal_control <= 1'b0;
-		
+
 				end
-		
-		
-				end	
+
+
+				end
 				end
 		end
 end
-	
-endmodule
 
+endmodule
--- spi_module.v
+++ spi_module.v
@@ -30,25 +30,27 @@ module spi_module
 	//Local registers and wires
 	reg is_ready_reg;
 	reg activate_ss;
+	/* verilator lint_off UNUSEDSIGNAL */
 	reg activate_sclk;
-	
+	/* verilator lint_on UNUSEDSIGNAL */
+
 	reg status_ignore_first_edge;
-	
+
 	wire rising_sclk_edge;
 	wire falling_sclk_edge;
-	
+
 	reg [SPI_WORD_LEN - 1:0] data_word_recv_reg;
-	
-	reg [SPI_WORD_LEN - 1:1] bit_counter;
-	
+
+	reg [SPI_WORD_LEN - 1:0] bit_counter;
+
 	reg [`SPI_MODULE_COMMAND_LEN - 1:0]spi_status;
 
 	assign is_ready = is_ready_reg;
-	
+
 	assign data_word_recv = data_word_recv_reg;
-	
+
 	assign processing_word = (spi_status == `SPI_STATUS_IDLE) ? 1'b0 : 1'b1;
-	
+
 	generate
 
 		if(SPI_MASTER) begin
@@ -64,86 +66,85 @@ module spi_module
 		end
 
 	endgenerate
-	
+
 	//Edge detector modules
 	pos_edge_det spi_edge_pos( .sig(SCLK_IN), .clk(master_clock), .pe(rising_sclk_edge));
 	neg_edge_det spi_edge_neg( .sig(SCLK_IN), .clk(master_clock), .ne(falling_sclk_edge));
-	
-	wire delay_pol =  (CPHA) ? ( (CPOL) ? (rising_sclk_edge) : (falling_sclk_edge)  ) : ( (CPOL) ? (SCLK_IN) : (!SCLK_IN) );	
-	
+
+	wire delay_pol =  (CPHA) ? ( (CPOL) ? (rising_sclk_edge) : (falling_sclk_edge)  ) : ( (CPOL) ? (SCLK_IN) : (!SCLK_IN) );
+
 	wire get_number_edge = (CPHA) ? ( (CPOL) ? (rising_sclk_edge) : (falling_sclk_edge) ) : ( (CPOL) ? (falling_sclk_edge) : (rising_sclk_edge) );
-	
+
 	wire switch_number_edge = (CPHA) ? ( (CPOL) ? (falling_sclk_edge) : (rising_sclk_edge) ) : ( (CPOL) ? (rising_sclk_edge) : (falling_sclk_edge) );
-	
+
 	wire SS = (SPI_MASTER) ? SS_OUT : SS_IN;
-	
+
 	assign OUTPUT_SIGNAL = (activate_ss) ? data_word_send[bit_counter] : 1'b0;
-	
+
 	always @(posedge master_clock) begin
-	
+
 		if (do_reset) begin
 			//do reset stuff
-			
+
 			activate_ss <= 1'b0;
-			
+
 			activate_sclk <= 1'b0;
-			
+
 			bit_counter <= (INVERT_DATA_ORDER) ? (0) : (SPI_WORD_LEN - 1);
-			
+
 			status_ignore_first_edge <= 1'b0;
-			
+
 			spi_status <= `SPI_STATUS_IDLE;
-			
+
 			is_ready_reg <= 1'b1;
-			
+
 		end
-		else begin		
+		else begin
 				case(spi_status)
-				
+
 					`SPI_STATUS_IDLE: begin
-						
+
 						if(process_next_word && delay_pol) begin
 							status_ignore_first_edge <= 1'b0;
 							activate_ss <= 1'b1;
-							activate_sclk <= 1'b1;	
-							spi_status <= `SPI_STATUS_CYCLE_BITS;	
+							activate_sclk <= 1'b1;
+							spi_status <= `SPI_STATUS_CYCLE_BITS;
 						end
-						
+
 					end
 
 					`SPI_STATUS_CYCLE_BITS: begin
-	
+
 						if(!SS) begin
-						
+
 							if(get_number_edge) data_word_recv_reg[bit_counter] <= INPUT_SIGNAL;
-	
+
 							if(switch_number_edge) begin
-						
+
 								if(CPHA && !status_ignore_first_edge) status_ignore_first_edge <= 1'b1;
 								else begin
-								
+
 									if(bit_counter ==  ((INVERT_DATA_ORDER) ? (SPI_WORD_LEN -1) : ('sd0)) ) begin //Word processed, reset
-								
+
 										activate_ss <= 1'b0;
 										activate_sclk <= 1'b0;
 										bit_counter <= (INVERT_DATA_ORDER) ? (0) : (SPI_WORD_LEN - 1);
 										spi_status <= `SPI_STATUS_IDLE;
-									
+
 									end
-									else bit_counter <= (INVERT_DATA_ORDER) ? (bit_counter + 1) : (bit_counter - 1);	
-																	
+									else bit_counter <= (INVERT_DATA_ORDER) ? (bit_counter + 1) : (bit_counter - 1);
+
 								end
 							end
-						
+
 						end
-						
+
 					end
-								
+
 				endcase
-				
+
 			end
-		
+
 	end
-		
+
 endmodule
-	
